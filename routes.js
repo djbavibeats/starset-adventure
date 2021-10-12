2bc80985fb7284c15d1962753269e85ad591ac6d
djbavibeats <justin@voltcreative.com>
2021-10-12 18:41:06 -0400

    make it live

--- routes.js
+++ routes.js
@@ -6,4 +6,17 @@ router.get("/", function(req, res) {
   res.render("index");
 });
 
+router.get("/island", function(req, res) {
+  res.render("island");
+})
+
+router.get("/mall", function(req, res) {
+  res.render("mall");
+})
+
+router.get("/plaza", function(req, res) {
+  res.render("plaza");
+})
+
+
 module.exports = router;
