a5a339e35b67ab42aded94f36140bc9b2f54ecf0
djbavibeats <justin@voltcreative.com>
2021-11-04 17:31:41 -0400

    add og tags and updated optimize message, make all pages draggable on desktop

--- public/main.js
+++ public/main.js
@@ -21,22 +21,29 @@ AFRAME.registerComponent('ios-video-fix', {
   }
 
   // Check to see if device is accessing site on mobile
-$(window).resize(function() {
-  if (window.location.pathname !== "/") {
+// $(window).resize(function() {
+  if (window.location.pathname == "/") {
     if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         if (window.matchMedia("(orientation: portrait)").matches) {
             // you're in PORTRAIT mode
+            document.querySelector('.optimize-modal').style.display = 'none';
         }
   
         if (window.matchMedia("(orientation: landscape)").matches) {
+            document.querySelector('.optimize-modal').style.display = 'none';
             // you're in LANDSCAPE mode
-            alert("Error! Please re-initialize experience on a mobile device in portrait mode.")
-            window.location.href = "/";
+            // alert("Error! Please re-initialize experience on a mobile device in portrait mode.")
+            // window.location.href = "/";
         }
     } else {
-      alert("Error! Please re-initialize experience on a mobile device in portrait mode.")
-      window.location.href = "/";
+      // alert("Error! Please re-initialize experience on a mobile device in portrait mode.")
+      // window.location.href = "/";
+      document.querySelector('.optimize-modal').style.display = 'flex';
     }
   }
-}).resize();
+// }).resize();
+
+document.getElementById('optimize-modal-close').addEventListener('click', function() {
+  document.querySelector('.optimize-modal').style.display = 'none';
+})
 
